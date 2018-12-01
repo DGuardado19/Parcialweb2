8790e72369b6c58f9cee6894fa31a663c962436b
DGuardado <00184217>
2018-12-01 16:05:40 -0600

    corrigiendo errores

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+require('./config/database');
+app.set('view engine', "ejs");
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/tareas.js
+++ routes/tareas.js
@@ -1,1 +1,9 @@
-async function(req,res,next)
+
+const express = require('express');
+const router = express.Router();
+router.get ('/', function(req,res,next){
+    return res.status(200).json({
+        "tarea":"realizada"
+    });
+})
+module.exports = router;
