408a25aa72b6ca1dc918f27826647a750b06d9f2
DGuardado <00184217>
2018-12-01 14:53:19 -0600

    terminando controladores

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,3 +2,27 @@ const user = require('../models/users');
 const userController = {};
 
 
+
+userController.index = async function(req,res,next) {
+
+    let user = await user.find();
+    return res.status(200).json(user)}
+
+userController.findUser = async function(req,res,next){
+    let {id}= req.params;
+    let user = await user.findById(id).catch(err=>{
+        return next(res);});
+        return res.status(200).json(user);}
+
+userController.stores = async function(req,res,next){
+    let user= new user;
+    user.nombre = req.body.user;
+    user.apellido = req.body.user;
+    try{
+        await user.save();
+        return res.status(200).json({"mensaje": "usuario agregado bb"});}
+
+        catch(err){
+            return res.status(500).json({err:err, menssage: "NO se pudo agregar amigo"});
+        }
+}
