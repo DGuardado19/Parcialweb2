cbe52795eb65845017a9acaf1a00df3c27f9d76e
DGuardado <00184217>
2018-12-01 18:01:02 -0600

    Agregando estilo e iconos

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,13 +6,14 @@ fetch("/users/", {method: "GET"})
     let filas ="";
     data.forEach(element => {
         filas = filas +  `<tr>
-        <td> ${element._id}</td>
-        <td> ${element.nombre}</td>
-        <td> ${element.apellido}</td>
-        <td> ${element.celular}</td>
-        <td>
-            <a href="">Actualizar </a>
-            <a href="">eliminar </a>
+        <td class="table-primary"> ${element._id}</td>
+        <td class="table-secondary"> ${element.nombre}</td>
+        <td class="table-success"> ${element.apellido}</td>
+        <td class="table-warning"> ${element.celular}</td>
+        <td class="table-danger">
+            <a href=""><i class="fas fa-pen-alt"></i> </a>
+            <a href="#">  <i class="fas fa-trash-alt"></i>
+            </a>
             </td>
             </tr>`
 
